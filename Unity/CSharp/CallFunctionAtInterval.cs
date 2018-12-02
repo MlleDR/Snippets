0f74621db10bf65a58d4e75564ea40c88c55e122
Mlle DR <mllelafortune@gmail.com>
2018-12-02 21:51:23 +0100

    Create CallFunctionAtInterval.cs

---
+++ Unity/CSharp/CallFunctionAtInterval.cs
@@ -0,0 +1,15 @@
+/***********************************************
+* Method to call a function at a given interval.
+************************************************/
+void CallFunctionAtInterval(string function, float startAt, float every)
+{
+    InvokeRepeating(function, startAt, everySeconds); // ex: Calls function DoSomething() after startAt seconds, and every every seconds
+}
+
+/*****************
+* Example function
+******************/
+void DoSomething()
+{
+    Debug.Log("DoSomething called");
+}
