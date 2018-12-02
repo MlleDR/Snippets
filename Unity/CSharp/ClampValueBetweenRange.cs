bfc235d63d65b62f8f61fd3c4eb9b231200d4b22
Mlle DR <mllelafortune@gmail.com>
2018-12-02 13:27:30 +0100

    Create ClampValueBetweenRange.cs
    Method to map a value between a given range.

---
+++ Unity/CSharp/ClampValueBetweenRange.cs
@@ -0,0 +1,19 @@
+/**********************************************
+ * Method to map a value between a given range.
+ * ********************************************/
+float MapValueToRange(float value, float min, float max)
+{
+    return Mathf.Clamp(value, min, max);
+}
+
+/*****************************
+ * Ex : Clamp the Input values
+ * ***************************/
+float InputValue = Input.GetAxis("Horizontal");
+float clampedInput = MapValueToRange(InputValue, 0, 1);
+
+/************************
+ * Ex : Change Health Bar
+ * **********************/
+float HealthDamage = 50;
+float clampedBar = MapValueToRange(HealthDamage, 0, 100);
