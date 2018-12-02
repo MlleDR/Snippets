39474b3d021050314219de84461cc243b1519554
Mlle DR <mllelafortune@gmail.com>
2018-12-02 12:19:19 +0100

    Update RandomToggle.js

--- Javascript/RandomToggle.js
+++ Javascript/RandomToggle.js
@@ -1,6 +1,6 @@
-/****************************************************
-* Method to toggle randomly between two given values.
-*****************************************************/
+/******************************************************
+* Method to toggle randomly between two specific values.
+******************************************************/
 function RandomToggle(value1, value2) {
     return [value1, value2][Math.round(Math.random())];
 }
