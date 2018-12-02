c7624ad4cf596c407dc3bfd687514ffc426078b3
Mlle DR <mllelafortune@gmail.com>
2018-12-02 21:55:59 +0100

    Update MapValueToRange.cs

--- Unity/CSharp/MapValueToRange.cs
+++ Unity/CSharp/MapValueToRange.cs
@@ -6,10 +6,10 @@ float MapValueToRange(float value, float min, float max)
     return Mathf.Clamp(value, min, max);
 }
 
-/* Ex : Clamp the Input values */
+// Ex : Clamp the Input values
 float InputValue = Input.GetAxis("Horizontal");
 float clampedInput = MapValueToRange(InputValue, 0, 1);
 
-/* Ex : Change Health Bar */
+// Ex : Change Health Bar
 float HealthDamage = 0.6f;
 float clampedBar = MapValueToRange(HealthDamage, 0, 100);
