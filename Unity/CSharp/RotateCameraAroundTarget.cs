ddbf3012be53229fb1aab78abbfc5ed80b8e7a3b
Mlle DR <mllelafortune@gmail.com>
2019-07-27 12:10:13 +0200

    [Add] Replaced hardcoded Keycode with custom keycodes.

--- Unity/CSharp/RotateCameraAroundTarget.cs
+++ Unity/CSharp/RotateCameraAroundTarget.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateCameraAroundTarget : MonoBehaviour
 {
     public Transform target;
+    public KeyCode leftArrow, rightArrow;
     public float rotationSpeed = 100f;
     private GameObject cameraOrbitTarget;
 
@@ -15,35 +16,35 @@ public class RotateCameraAroundTarget : MonoBehaviour
             PrepareOrbitStructure();
         }
     }
-    
+
     void Update()
     {
         RotateWithKeyboardInput();
     }
-  
+
     void PrepareOrbitStructure()
     {
         // creates an empty gameObject at target position
-        cameraOrbitTarget = new GameObject(); 
+        cameraOrbitTarget = new GameObject();
         cameraOrbitTarget.transform.position = target.transform.position;
-        
+
         // name the new Empty gameObject
-        cameraOrbitTarget.name = "CAMERA_TARGET_OF[" + target.name + "]"; 
-        
+        cameraOrbitTarget.name = "CAMERA_TARGET_OF[" + target.name + "]";
+
         // parent the object on which you've put the script (here the camera) to new Empty gameObject
         transform.parent = cameraOrbitTarget.transform;
     }
-    
+
     void RotateWithKeyboardInput()
     {
         float YRotation = rotationSpeed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(leftArrow))
         {
             cameraOrbitTarget.transform.Rotate(0f, YRotation, 0f);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(rightArrow))
         {
             cameraOrbitTarget.transform.Rotate(0f, -YRotation, 0f);
         }
