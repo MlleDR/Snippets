b0c232e14e3aa7e2bc5dfbe856480877ce678448
Mlle DR <mllelafortune@gmail.com>
2021-04-26 09:02:26 +0200

    Update AddComponent_TextMesh.cs

--- Unity/CSharp/AddComponent_TextMesh.cs
+++ Unity/CSharp/AddComponent_TextMesh.cs
@@ -15,18 +15,13 @@ public class AddComponent_TextMesh : MonoBehaviour
     public float characterSize = 0.01f;
     public int fontSize = 80;
 
-
     private TextMesh textmesh;
-
-
 
     private void Start()
     {
         // Add a TextMesh Component in Realtime on a gameobject
         DisplayText(text, characterSize, fontSize);
     }
-
-
 
     // Add a TextMesh Component in Realtime on a gameobject
     private void DisplayText(/*optional*/ string Text, float CharacterSize, int FontSize)
