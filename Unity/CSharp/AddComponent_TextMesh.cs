ec0153a9acedc0c11d31508342d0df8ddadd3975
Mlle DR <mllelafortune@gmail.com>
2021-04-26 09:01:28 +0200

    Simplify code

--- Unity/CSharp/AddComponent_TextMesh.cs
+++ Unity/CSharp/AddComponent_TextMesh.cs
@@ -1,30 +1,43 @@
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
+// AddComponent_TextMesh script //
+/* 26.04.2021 - Simplify code
+ * 25.04.2021 - Added script
+ */
+
+[RequireComponent(typeof(MeshRenderer))] // TesxtMesh needs MeshRenderer. Here I force its creation, but you can add it manually or make sure your gameobject has one.
 public class AddComponent_TextMesh : MonoBehaviour
 {
+    public string text = "My realtime text";
+
+    /* Tips : 
+     * Decrease Character Size and 
+     * increase FontSize to get a nice text render */
+    public float characterSize = 0.01f;
+    public int fontSize = 80;
+
+
     private TextMesh textmesh;
-    private MeshRenderer rend;
-    private string text = "Realtime TextMesh";
-    private float characterSize = 0.16f;
+
 
 
     private void Start()
     {
         // Add a TextMesh Component in Realtime on a gameobject
-        DisplayText(text, characterSize);
+        DisplayText(text, characterSize, fontSize);
     }
 
 
+
     // Add a TextMesh Component in Realtime on a gameobject
-    private void DisplayText(/*optional*/ string Text, float CharacterSize)
+    private void DisplayText(/*optional*/ string Text, float CharacterSize, int FontSize)
     {
-        rend = transform.GetComponent<MeshRenderer>();
         textmesh = transform.gameObject.AddComponent<TextMesh>();
 
         // Optional
         textmesh.anchor = TextAnchor.MiddleCenter;
         textmesh.text = Text;
         textmesh.characterSize = CharacterSize;
+        textmesh.fontSize = FontSize;
     }
 }
