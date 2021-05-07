4d471023d466932cf407af2f7711286c8b5d5346
Mlle DR <mllelafortune@gmail.com>
2021-05-07 20:03:29 +0200

    Fixed attributes - Transform to Vector3

--- Unity/CSharp/ConstantLerp.cs
+++ Unity/CSharp/ConstantLerp.cs
@@ -2,7 +2,7 @@
 // Constant Lerp from A to B with speed
 
 private float moveDir;
-private void ConstantLerp(Transform from, Transform to, float speed)
+private void ConstantLerp(Vector3 from, Vector3 to, float speed)
 {
     moveDir = speed * (1 / Vector3.Distance(from.position, to.position) * Time.deltaTime);
     transform.position = Vector3.Lerp(from.position, to.position, moveDir);
